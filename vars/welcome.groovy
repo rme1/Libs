8d11b79a1afdb0ce3e21de587863a5509886bbcd
rme1 <merznicht@gmx.de>
2021-11-26 18:42:08 +0100

    tmp funktionen entfernt

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,18 +1,3 @@
-def fnWelcome(pName){
-    echo "Ralf Merznicht ist ${pName}"
-}
-
-def fnRunPowerShell(pName){
-    echo "Ralf Merznicht ist ${pName}"
-    //withEnv(["ParamInPowershell=${env:TIMEZONE}"])
-    //{    
-        powershell script{'''
-            Write-Output ${env:TIMEZONE}
-        '''
-        }
-    //}
-}
-
 def WaitForNextTimeZone() {
     powershell script: '''
         
